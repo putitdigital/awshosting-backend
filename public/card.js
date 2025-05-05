abbb9ff6ce21f3935f95757cde098871556e2952
sithembiso <putitdigital@gmail.com>
2025-05-05 14:30:14 +0200

    latest code

---
+++ public/card.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+    var sync1 = $("#sync1");
+
+    var slidesPerPage = 4; //globaly define number of elements per page
+    var syncedSecondary = true;
+  
+    sync1.owlCarousel({
+      items : 1,
+      slideSpeed : 2000,
+      nav: true,
+      autoplay: true,
+      dots: true,
+      loop: true,
+      responsiveRefreshRate : 200,
+      navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+    }).on('changed.owl.carousel', syncPosition);
+  
+    
+  
+    function syncPosition(el) {
+      //if you set loop to false, you have to restore this next line
+      //var current = el.item.index;
+      
+      //if you disable loop you have to comment this block
+      var count = el.item.count-1;
+      var current = Math.round(el.item.index - (el.item.count/2) - .5);
+      
+      if(current < 0) {
+        current = count;
+      }
+      if(current > count) {
+        current = 0;
+      }
+    }
+    
+    function syncPosition2(el) {
+      if(syncedSecondary) {
+        var number = el.item.index;
+        sync1.data('owl.carousel').to(number, 100, true);
+      }
+    }
+  });
