e6649a6509ceaf0e65eb682f51b5f556f1bf4f8c
sithembiso <putitdigital@gmail.com>
2025-05-05 23:42:39 +0200

    email

--- routes/routes.js
+++ routes/routes.js
@@ -4,10 +4,6 @@ router.get("/", (req, res) =>{
    res.render("index")
 })
 router.post("/connect", (req, res) =>{
-   var firstName = req.body.firstName;
-   var lastName = req.body.lastName;
-   var email = req.body.email;
-   var message = req.body.message;
    res.render("connect")
 })
 
